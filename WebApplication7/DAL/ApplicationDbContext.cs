40f1eabe2cbd519ede95f994abbf41f8ba9ae85a
george <geo.xiros@gmail.com>
2019-01-31 20:24:00 +0200

    add mvc

---
+++ WebApplication7/DAL/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿
+namespace WebApplication7.DAL
+{
+    public class ApplicationDbContext : System.Data.Entity.DbContext
+    {
+        public ApplicationDbContext() : base("server=.\\sqlexpress;database=eShopMvc;user id=sa;password=1234")
+        {
+        }
+        System.Data.Entity.DbSet<Models.User> Users { get; set; }
+        System.Data.Entity.DbSet<Models.Cart> Carts { get; set; }
+        System.Data.Entity.DbSet<Models.Orders> Orders { get; set; }
+    }
+}
---
+++ WebApplication7/Migrations/201901311721234_init.cs
@@ -0,0 +1,51 @@
+namespace WebApplication7.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class init : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Carts",
+                c => new
+                    {
+                        id = c.Int(nullable: false, identity: true),
+                        order_id = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.id);
+            
+            CreateTable(
+                "dbo.Orders",
+                c => new
+                    {
+                        id = c.Int(nullable: false, identity: true),
+                        user_id = c.Int(nullable: false),
+                        product_id = c.Int(nullable: false),
+                        date = c.DateTime(nullable: false),
+                        quantity = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.id);
+            
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        id = c.Int(nullable: false, identity: true),
+                        name = c.String(),
+                        username = c.String(),
+                        password = c.String(),
+                    })
+                .PrimaryKey(t => t.id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Users");
+            DropTable("dbo.Orders");
+            DropTable("dbo.Carts");
+        }
+    }
+}
